e24205467858d7a47c90fb6411e3e497ffefb614
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-01-28 16:58:10 -0600

    add prevGuessses to game

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log('guess the number lab')
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: /*1. Add a `prevGuesses` property to the `game` object initialized to an empty array.*/
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
